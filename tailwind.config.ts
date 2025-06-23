e849d7ff478f15d8dc28027b207cd6a77eb1cd82
Matheus Willock <matheuswillock@gmail.com>
2025-06-23 20:54:26 -0300

    feat: update package version to 1.0.0 and modify dependencies
    - Changed version from 0.1.0 to 1.0.0 in package.json
    - Removed turbopack from dev script
    - Added new scripts for API and testing
    - Updated dependencies: added axios, fs-extra, minimist, whatsapp-web.js, and xlsx
    - Updated devDependencies: added autoprefixer, postcss, and updated typescript version
    
    style: enhance global styles with new color variables and dark mode support
    
    - Updated CSS variables for light and dark themes
    - Added global styles for body and headings
    - Removed commented-out theme styles
    
    chore: improve TypeScript configuration
    
    - Added strictNullChecks and types for node and next in tsconfig.json
    
    feat: create Axios configuration file
    
    - Added AxiosConfig.ts for API configuration
    
    feat: initialize Tailwind CSS configuration
    
    - Created tailwind.config.ts with custom theme settings and dark mode support

---
+++ tailwind.config.ts
@@ -0,0 +1,70 @@
+// tailwind.config.ts
+import type { Config } from 'tailwindcss';
+
+export default {
+  darkMode: 'class',
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+			fontWeight: {
+				light: '300',
+				normal: '400',
+				medium: '500',
+				semibold: '600',
+				bold: '700',
+				extrabold: '800',
+				black: '900',
+			},
+      fontFamily: {
+        sans: ['Nunito', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        'h1': ['52px', '52px'],   // Título h1
+        'h2': ['32px', '32px'],   // Título h2
+        'h3': ['28px', '20px'],   // Título h3
+      },
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        card: 'var(--card)',
+        'card-foreground': 'var(--card-foreground)',
+        popover: 'var(--popover)',
+        'popover-foreground': 'var(--popover-foreground)',
+        primary: 'var(--primary)',
+        'primary-foreground': 'var(--primary-foreground)',
+        secondary: 'var(--secondary)',
+        'secondary-foreground': 'var(--secondary-foreground)',
+        muted: 'var(--muted)',
+        'muted-foreground': 'var(--muted-foreground)',
+        accent: 'var(--accent)',
+        'accent-foreground': 'var(--accent-foreground)',
+        destructive: 'var(--destructive)',
+        border: 'var(--border)',
+        input: 'var(--input)',
+        ring: 'var(--ring)',
+        'chart-1': 'var(--chart-1)',
+        'chart-2': 'var(--chart-2)',
+        'chart-3': 'var(--chart-3)',
+        'chart-4': 'var(--chart-4)',
+        'chart-5': 'var(--chart-5)',
+        sidebar: 'var(--sidebar)',
+        'sidebar-foreground': 'var(--sidebar-foreground)',
+        'sidebar-primary': 'var(--sidebar-primary)',
+        'sidebar-primary-foreground': 'var(--sidebar-primary-foreground)',
+        'sidebar-accent': 'var(--sidebar-accent)',
+        'sidebar-accent-foreground': 'var(--sidebar-accent-foreground)',
+        'sidebar-border': 'var(--sidebar-border)',
+        'sidebar-ring': 'var(--sidebar-ring)',
+      },
+      animation: {},
+      keyframes: {},
+			dropShadow: {}
+    }
+  }
+} satisfies Config;
